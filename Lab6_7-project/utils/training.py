73bc2b2d67e64eb8d708b3e216eaa6f8ec07bab5
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2026-04-20 11:54:09 +0200

    WIP: Training Utils, train one epoch along with norm clipping , val one epoch

---
+++ Lab6_7-project/utils/training.py
@@ -0,0 +1,33 @@
+import torch
+from torch.nn.utils import clip_grad_norm_
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def _train_one_epoch(model, loader, optimizer, criterion, max_norm:float=1.0):
+    model.train()
+    running_loss = 0.0
+    for X, y in loader:
+        X, y = X.to(DEVICE), y.to(DEVICE)
+        optimizer.zero_grad()
+        loss = criterion(model(X), y)
+        running_loss += loss.item()
+        loss.backward()
+        clip_grad_norm_(model.parameters(), max_norm=max_norm)
+        optimizer.step()
+    avg_loss = running_loss / len(loader)
+
+    return avg_loss
+
+def _calculate_val_loss(model, loader, criterion):
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for X,y in loader:
+            X, y = X.to(DEVICE), y.to(DEVICE)
+            output = model(X)
+            loss = criterion(output, y)
+            running_loss += loss.item()
+    avg_loss = running_loss / len(loader)
+
+    return avg_loss
