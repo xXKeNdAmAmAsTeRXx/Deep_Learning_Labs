b74df9f9bd4ee7ef6c5bdf4bd38bebe7b4307ee8
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2026-04-28 06:15:47 +0200

    create init with model loading and mean response ensemble

--- Lab6_7-project/utils/Predictor.py
+++ Lab6_7-project/utils/Predictor.py
@@ -1,18 +1,40 @@
 from typing import Literal
+import os
 
+import numpy as np
 import torch
+from MLPClassifier import MLPClassifier
+
 # TODO: Imports
 
 
 # TODO: static Typing
 class Predictor:
-    def __init__(self):
-        # TODO: Write init function - keep models or model paths
-        pass
+    def __init__(self, path:str, model_params:dict) -> None:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+        folds_path = os.listdir(path)
+        self.folds = []
+        for f in folds_path:
+            m = MLPClassifier(**model_params)
+            m.load_state_dict(torch.load(os.path.join(path, f)))
+            self.folds.append(m)
 
-    def _mean_response_ensemble(self):
-        pass
+        self.in_out_dim = (model_params['input_dim'], model_params['output_dim'])
+
+    def _mean_response_ensemble(self, data:np.ndarray) -> np.ndarray:
+        data_t = torch.tensor(data)
+        probs = np.zeros(self.in_out_dim[0])
+        for idx, f in enumerate(self.folds):
+            f.eval()
+            with torch.no_grad():
+                outputs = f(data_t)
+                proba = torch.softmax(outputs, dim=0).numpy()
+
+            mean_proba = np.mean(proba, axis=0)
+            winner = mean_proba.argmax()
+
+        return winner
 
     def _majority_voting_ensemble(self):
         pass
